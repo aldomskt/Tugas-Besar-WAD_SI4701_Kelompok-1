176f7b7948ddd763fb3be78b436bafa8cf801a7c
biannso <biannso4@gmail.com>
2025-06-01 23:37:15 +0700

    Push Pelanggan

---
+++ manajemen-penitipan-kendaraan-kelompok-1/app/Models/Pelanggan.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class Pelanggan extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable;
+
+    protected $fillable = [
+        'nama',
+        'email',
+        'password',
+        'nim',
+        'alamat',
+        'no_hp',
+        'no_ktp',
+        'foto'
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
+
+    public function kendaraans()
+    {
+        return $this->hasMany(Kendaraan::class);
+    }
+
+    public function transaksis()
+    {
+        return $this->hasMany(Transaksi::class);
+    }
+}
